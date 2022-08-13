f3038bac1e4f14b33ffd507668a055912b5e2c20
unknown <sadiajannatdisha19@gmail.com>
2022-08-13 13:10:28 +0600

    vv

--- index.js
+++ index.js
@@ -8,16 +8,14 @@ var app=express();
 app.use(express.static('/public'));
 app.set('view engine','ejs');
 
-function connection(){
-    return mysql.createConnection({
-        host:'localhost',
-        user:'root',
-        password:'',
-        database:'sample'
-    });
-}
+var con=mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'bus_management'
+});
 
-app.listen(8000);
+app.listen(3000);
 app.use(bp.urlencoded({extends:true}));
 app.use(express.static(__dirname + "/public"));
 app.use(express.static("."));
@@ -27,10 +25,102 @@ app.use(express.static("."));
 //routes
 
 app.get('/',function(req,res){
-    return res.render('index',);   
+    return res.render('index');   
 });
+app.get('/ads',function(req,res){
+    return res.render('index');   
+});
+app.get('/admin',function(req,res){
+    return res.render('Admin');   
+});
+app.get('/student',function(req,res){
+    return res.render('Student');   
+});
+app.get('/driver',function(req,res){
+    return res.render('Driver');   
+});
+app.get('/about',function(req,res){
+    return res.render('index');   
+});
+app.get('/contact',function(req,res){
+    return res.render('index');   
+});
+
+app.post('/register-student',(req,res)=>{
+    var name=req.body.names;
+    var email=req.body.emails;
+    var pass=req.body.passs;
+    var contact=req.body.contacts;
+    
+    var sql=`INSERT INTO students_details(Name,Email_Address,password,Contact_No) VALUES('${name}','${email}+','${pass}','${contact}');`;
+    
+    con.query(sql,(error,result)=>{
+        res.redirect('/schedule');
+    })
+});
+app.post('/register-driver',(req,res)=>{
+    var name=req.body.names;
+    var email=req.body.emails;
+    var pass=req.body.passs;
+    var contact=req.body.contacts;
+    
+    var sql=`INSERT INTO driver_details(Name,Email_Address,password,Contact_No) VALUES('${name}','${email}+','${pass}','${contact}');`;
+    
+    con.query(sql,(error,result)=>{
+        if(error){
+            res.send(error)
+        }
+        else{
+            res.redirect(`/driver/${email}`);
+        }   
+    })
+});
+
+app.post('/register-final-driver',(req,res)=>{
+    var name=req.body.names;
+    var email=req.body.emails;
+    var pass=req.body.passs;
+    var contact=req.body.contacts;
+    var id;
+    con.query(`SELECT COUNT(*) AS n FROM driver_details`,(e,r)=>{
+        id=r[0].n+1;
+    });
+    
+    var sql=`INSERT INTO driver_details(Driver_ID,Name,Email_Address,password,Contact_No) VALUES(${id},'${name}','${email}+','${pass}','${contact}');`;
+    
+    con.query(sql,(error,result)=>{
+        if(error){
+            res.send(error)
+        }
+        else{
+            res.redirect(`/driver/schedule/${id}`);
+        }   
+    })
+});
+app.get('/driver/schedule/:id',(req,res)=>{
+    var id=req.params.id;
+    con.query(`SELECT * FROM bus_schedule JOIN driver_details ON bus_schedule.Driver_ID=driver_details.Driver_ID WHERE Driver_ID='${id}'`,(e,r)=>{
+        return res.render('Schedule',{
+        schedule:r
+    });
+    }) 
+})
+
+app.get('/driver/:email',(req,res)=>{
+    return res.render('Driver-final.ejs');
+});
+
+app.get('/schedule',(req,res)=>{
+    con.query('SELECT * FROM bus_schedule JOIN driver_details ON bus_schedule.Driver_ID=driver_details.Driver_ID',(e,r)=>{
+        return res.render('Schedule',{
+        schedule:r
+    });
+    })    
+});
+
+
+
 app.post('/home',function(req,res){
-    var con=connection();
     var email=req.body.email;
     var pass=req.body.pass;
     var sql="SELECT id FROM users WHERE email='"+email+"' AND password='"+pass+"'";
@@ -43,6 +133,30 @@ app.post('/home',function(req,res){
             return res.send('invalid email or password');
         }
     })  
+});
+app.post('/student-login',function(req,res){
+    var email=req.body.emaill;
+    var pass=req.body.passl;
+    con.query(`SELECT * FROM students_details WHERE Email_Address='${email}' AND password='${pass}'`,(e,r)=>{
+        if(r){
+            return res.render('/schedule');
+        }
+        else{
+            return res.redirect('back');
+        }
+    })
+});
+app.post('/driver-login',function(req,res){
+    var email=req.body.emaill;
+    var pass=req.body.passl;
+    con.query(`SELECT * FROM driver_details WHERE Email_Address='${email}' AND password='${pass}'`,(e,r)=>{
+        if(r){
+            return res.render('/driver/schedule/:id');
+        }
+        else{
+            return res.redirect('back');
+        }
+    })
 });
 
 app.get('/login',function(req,res){
